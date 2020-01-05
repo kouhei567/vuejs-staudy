1944450102345101186eb646b65ebb0d021cf144
kouheidev <kouheidev@gmail.com>
2020-01-05 23:15:03 +0900

    vccode

---
+++ vuenyuumon/01-01.宣言的レンダリング/index.js
@@ -0,0 +1,16 @@
+var app = new Vue({ 
+    el: '#app',
+    data: {
+        message: 'Hello Vue!'
+    }
+});
+
+var app2 = new Vue({ 
+    el: '#app-2',
+    data: {
+        message: 'You loaded this page on ' + new Date().toLocaleString()
+    }
+});
+
+// メッセージの上書きする
+app.message = 'I have changed the data!ここで';
